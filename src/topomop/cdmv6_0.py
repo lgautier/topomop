0accab1bb0033b40e903b40a525028c4e47c86f7
Laurent Gautier <lgautier@gmail.com>
2026-02-13 15:58:12 -0500

    Ensure that `PROCEDURE_OCCURRENCE.VISIT_DETAIL_ID` a foreign key.
    This is for CDM version 5.3 and 6.0, and this is added as a patch
    until https://github.com/OHDSI/CommonDataModel/issues/777 is fixed
    upstream.

--- src/topomop/cdmv6_0.py
+++ src/topomop/cdmv6_0.py
@@ -133,7 +133,13 @@ _PATCH_OVERRIDE_ATTRIBUTES: dict[str, _TYPE_PATCH_OVERRIDE_ATTRIBUTES] = {
                 (
                     ('isForeignKey', True),  # fkTableName and fkFieldName are already set.
                 ),
-                'https://github.com/OHDSI/CommonDataModel/issues/776'
+                'See https://github.com/OHDSI/CommonDataModel/issues/776'
+            ),
+            'VISIT_DETAIL_ID': (
+                (
+                    ('isForeignKey', True),  # fkTableName and fkFieldName are already set.
+                ),
+                'See https://github.com/OHDSI/CommonDataModel/issues/777'
             )
         }
     }
